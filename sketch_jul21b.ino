2b9befed34124fb0ab4dabe343769a9d94dfbfee
doabell <35297086+doabell@users.noreply.github.com>
2018-07-22 09:22:51 +0800

    Add files via upload
    files used during hackathon.

---
+++ sketch_jul21b.ino
@@ -0,0 +1,19 @@
+ const int buttonPin = 3;    
+  const int ledPin =  13;      
+  void setup() {
+    pinMode(ledPin, OUTPUT);      
+    pinMode(buttonPin, INPUT);
+    Serial.begin(9600);     
+  }
+  void loop()
+  {
+    Serial.println(digitalRead(buttonPin));
+    delay(20);
+     if (digitalRead(buttonPin) == LOW) 
+     {     
+       digitalWrite(ledPin, HIGH);  
+     } 
+     else {
+       digitalWrite(ledPin, LOW); 
+     }
+  }
